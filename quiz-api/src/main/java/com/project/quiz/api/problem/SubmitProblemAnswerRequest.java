94ebcdcd10a063c65d9db991a2b7e9061be11b5f
rivkode <jonghuncu@gmail.com>
2026-03-28 14:51:54 +0900

    #15 refactor: consolidate problem api controller

--- quiz-api/src/main/java/com/project/quiz/api/problem/SubmitProblemAnswerRequest.java
+++ quiz-api/src/main/java/com/project/quiz/api/problem/SubmitProblemAnswerRequest.java
@@ -1,4 +1,4 @@
-package com.project.quiz.api.solving;
+package com.project.quiz.api.problem;
 
 import com.project.quiz.domain.problem.ProblemAnswerFormat;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
